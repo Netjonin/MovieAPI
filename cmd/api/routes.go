9439aa0a29e58917e1fc3557a04871fc72b5e950
netjonin <pencilpepeyo@yahoo.com>
2024-01-25 23:15:31 +0100

    user handler and route created

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,9 +9,9 @@ import (
 func (app *application) routes() http.Handler {
 
 	router := httprouter.New()
-	
+
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
-	
+
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
@@ -19,5 +19,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	return app.recoverPanic(app.rateLimit(router))
 }
