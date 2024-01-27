db6f7e8bfa1e7672268afcc7d81aeb92f6121a67
netjonin <pencilpepeyo@yahoo.com>
2024-01-27 14:31:24 +0100

    port number changed for sending emails on user signup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -63,8 +63,8 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
-	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
-	flag.IntVar(&cfg.smtp.port, "smtp-port", 465, "SMTP port")
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "7950205b99f449", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "a1d2481b1bc422", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
