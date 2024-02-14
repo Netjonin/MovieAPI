aafd16e8ecbf54193a3829cf2580ae0b5222385c
netjonin <pencilpepeyo@yahoo.com>
2024-02-14 07:27:40 +0100

    User Authorization splitted into authentication and activation checks

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -88,29 +88,29 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 func (app *application) authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Authorization")
-		
+
 		authorizationHeader := r.Header.Get("Authorization")
-		
+
 		if authorizationHeader == "" {
 			r = app.contextSetUser(r, data.AnonymousUser)
 			next.ServeHTTP(w, r)
 			return
 		}
-		
+
 		headerParts := strings.Split(authorizationHeader, " ")
 		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
 			app.invalidAuthenticationTokenResponse(w, r)
 			return
 		}
-		
+
 		token := headerParts[1]
 		v := validator.New()
-		
+
 		if data.ValidateTokenPlaintext(v, token); !v.Valid() {
 			app.invalidAuthenticationTokenResponse(w, r)
 			return
 		}
-		
+
 		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
 		if err != nil {
 			switch {
@@ -121,26 +121,33 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 			}
 			return
 		}
-		
+
 		r = app.contextSetUser(r, user)
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		
 		user := app.contextGetUser(r)
-		
 		if user.IsAnonymous() {
 			app.authenticationRequiredResponse(w, r)
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
 		if !user.Activated {
 			app.inactiveAccountResponse(w, r)
 			return
 		}
 		next.ServeHTTP(w, r)
 	})
+	return app.requireAuthenticatedUser(fn)
 }
