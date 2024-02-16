160e4ec511fd0191c735c142186aa4b9a1f6a39e
netjonin <pencilpepeyo@yahoo.com>
2024-02-16 09:28:47 +0100

    Access-Control-Allow-Origin functionality for trusted origins

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,10 @@ type config struct {
 		password string
 		sender   string
 	}
+
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -70,6 +75,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "7950205b99f449", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "a1d2481b1bc422", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
